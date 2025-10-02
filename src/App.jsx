b54182e423b2c9dae66855f51df9020cab4346b7
Carlos <cvidalesb@gmail.com>
2025-10-02 14:19:35 +0200

    remove home page

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Router>
         <div className="App">
           <Routes>
-            <Route path="/" element={<Home />} />
+            {/* <Route path="/" element={<Home />} /> */}
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route 
